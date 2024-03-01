50e87b1b771a128a96f5fda94f752b6bc07b9d3a
Yaser Ahmady <y@yaserahmady.com>
2024-03-01 17:05:44 +0100

    Add documentation explaining code

--- functions.php
+++ functions.php
@@ -2,13 +2,24 @@
 /**
  * Tema WordPress di base basato su Timber e
  * Advanced Custom Fields per Mohole.
+ *
+ * functions.php è il file di un tema WordPress
+ * che contiene tutte le funzioni specifiche al
+ * nostro tema.
+ * https://developer.wordpress.org/themes/basics/theme-functions/
  */
 
+// Questa espressione serve al nostro editor di
+// codice per capire che tipo di oggetti stiamo
+// utilizzando, in soldoni serve a far funzionare
+// l'Intellisense di VSCode.
 use Timber\Timber;
 
 // Importa le dipendenze installate con Composer.
 require_once __DIR__ . "/vendor/autoload.php";
 
+// Importa la classe di Timber\StarterSite per
+// inizializzarla dopo.
 require_once __DIR__ . "/includes/theme-timber.php";
 
 // Inizializza Timber
